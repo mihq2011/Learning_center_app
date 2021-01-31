67aab4d8725ca900e3b6f3586509c9bda4f6ff73
Mauricio Heredia <mihq1990@gmail.com>
2021-01-31 12:48:02 -0800

    bugs fixed

--- javascript.js
+++ javascript.js
@@ -4,3 +4,13 @@ sideMenu.addEventListener("click", function () {
     var sideM = document.querySelector('.side-menu');
     sideM.classList.toggle("active");
 });
+
+
+
+var bookClicked = document.querySelector('.book-card');
+
+bookClicked.addEventListener("click", function() {
+    var popUpFun = document.querySelector(".books-page-popup");
+    popUpFun.classList.toggle("active");
+});
+
